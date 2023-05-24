247852210ba90165997569c4eda35587e45c1edb
Kavish Gupta <btech10295.19@bitmesra.ac.in>
2023-05-25 01:57:08 +0530

    2 am update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,12 @@ import { styles } from "../styles";
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
+    //video background
     <video autoPlay loop muted playsInline style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', minWidth: '100%', minHeight: '100%' }}>
       <source src={videobg} type="video/mp4" />
     </video>
+
+    
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
@@ -18,31 +21,12 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+          <span className='text-[#964B00]'>The Glamping Groove</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            Groove int the wild
           </p>
         </div>
-      </div>
-
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
       </div>
     </section>
   );
